d2448eaa396de0625998aa605c3a53f8f5032f46
Iain Smith <b099l3@gmail.com>
2022-08-29 16:40:14 +0200

    added time now in oslo

--- lib/core/infrastructure/conf_data.dart
+++ lib/core/infrastructure/conf_data.dart
@@ -1,6 +1,13 @@
+import 'package:timezone/browser.dart' as tz;
 class ConfData {
   static List<DateTime> days = [
     DateTime(2022, 8, 31, 9, 00),
     DateTime(2022, 9, 1, 9, 00),
   ];
+
+  static DateTime get nowInConference {
+    var oslo = tz.getLocation('Europe/Oslo');
+    var nowOslo = tz.TZDateTime.now(oslo);
+    return nowOslo;
+  }
 }
